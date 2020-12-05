e18b46dd81587a91894d6b00b988ad3fd59e2715
Luke.Humphreys <lhumphreys67@gmail.com>
2020-12-05 23:29:52 +0000

    2020: Day 5

---
+++ tests/aoc2020Tests/test_aoc2020_d05.py
@@ -0,0 +1,49 @@
+from unittest import TestCase
+
+from aoc2020.d05 import chop, get_all_seats, getSeat
+
+
+class Test_Part1(TestCase):
+    def test_example1(self):
+        (row, col, seat_id) = getSeat("BFFFBBFRRR")
+        self.assertEqual(row, 70)
+        self.assertEqual(col, 7)
+        self.assertEqual(seat_id, 567)
+
+    def test_example2(self):
+        (row, col, seat_id) = getSeat("FFFBBBFRRR")
+        self.assertEqual(row, 14)
+        self.assertEqual(col, 7)
+        self.assertEqual(seat_id, 119)
+
+    def test_example3(self):
+        (row, col, seat_id) = getSeat("BBFFBBFRLL")
+        self.assertEqual(row, 102)
+        self.assertEqual(col, 4)
+        self.assertEqual(seat_id, 820)
+
+    def test_getSeats(self):
+        rows = [x for x in range(128)]
+        cols = [c for c in range(8)]
+        self.assertListEqual(get_all_seats()[0], rows)
+        self.assertListEqual(get_all_seats()[1], cols)
+
+    def test_FirstTest(self):
+        (rows, cols) = chop(get_all_seats(), "F")
+        self.assertListEqual(rows, [x for x in range(64)])
+        self.assertListEqual(cols, [x for x in range(8)])
+
+    def test_BottomHalf(self):
+        (rows, cols) = chop(get_all_seats(), "B")
+        self.assertListEqual(rows, [x for x in range(64, 128)])
+        self.assertListEqual(cols, [x for x in range(8)])
+
+    def test_LeftHalf(self):
+        (rows, cols) = chop(get_all_seats(), "L")
+        self.assertListEqual(rows, [x for x in range(128)])
+        self.assertListEqual(cols, [0, 1, 2, 3])
+
+    def test_RightHalf(self):
+        (rows, cols) = chop(get_all_seats(), "R")
+        self.assertListEqual(rows, [x for x in range(128)])
+        self.assertListEqual(cols, [4, 5, 6, 7])
