bee84083a9c9520a3a6f81084ed135bfdff57d9e
Luke.Humphreys <lhumphreys67@gmail.com>
2022-12-01 18:14:46 +0000

    AOC 2022 - Day 1
    Required adding support for new type of file laod (int groups), which
    behaves the same as the string groups, but converts to ints

--- aoc2022/d01.py
+++ aoc2022/d01.py
@@ -1,12 +1,28 @@
-from tools.file_loader import load_ints
+from tools.file_loader import load_int_groups
+from dataclasses import dataclass
 
 
-def last(numbers: list[int]) -> int:
-    return numbers[-1]
+@dataclass
+class Elf:
+    carrying: list[int]
+    total: int
+
+    def __lt__(self, other):
+        return self.total < other.total
+
+
+def load_elves(path: str):
+    return [Elf(carrying=group,
+                total=sum(group)) for group in load_int_groups(path)]
+
+
+def get_elves_with_most(elves: list[Elf], n: int) -> list[Elf]:
+    return sorted(elves)[n*-1:]
 
 
 if __name__ == "__main__":
     def main():
-        numbers = load_ints("input/d01.txt")
-        print(last(numbers))
+        elves = load_elves("input/d01.txt")
+        print(get_elves_with_most(elves, 1))
+        print(sum([elf.total for elf in get_elves_with_most(elves, 3)]))
     main()
