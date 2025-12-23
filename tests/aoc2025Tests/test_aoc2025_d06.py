8eb598e41447b4b8c882c84caf9649c57b78833c
Luke.Humphreys <lhumphreys67@gmail.com>
2025-12-23 14:45:54 -0500

    2025 d06 - Part 2

--- tests/aoc2025Tests/test_aoc2025_d06.py
+++ tests/aoc2025Tests/test_aoc2025_d06.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from aoc2025.d06 import load_sample, part1, solve_part_one
-from aoc2025.d06 import  part2
+from aoc2025.d06 import  part2, solve_part_two
 from tests.aoc2025Tests.aoc2025_common import get_test_file_path
 
 
@@ -15,8 +15,14 @@ class TestDay06(TestCase):
         self.assertEqual(solve_part_one(self.data[2]), 4243455)
         self.assertEqual(solve_part_one(self.data[3]), 401)
 
+    def test_solve_part_two(self):
+        self.assertEqual(solve_part_two(self.data[0]), 8544)
+        self.assertEqual(solve_part_two(self.data[1]), 625)
+        self.assertEqual(solve_part_two(self.data[2]), 3253600)
+        self.assertEqual(solve_part_two(self.data[3]), 1058)
+
     def test_part_one(self):
         self.assertEqual(part1(self.data), 4277556)
 
     def test_part_two(self):
-        self.assertEqual(part2(self.data), 0)
+        self.assertEqual(part2(self.data), 3263827)
