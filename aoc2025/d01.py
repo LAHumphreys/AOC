2785d91745d59b5a662ac29c3d35ef525c511752
Luke.Humphreys <lhumphreys67@gmail.com>
2025-12-22 19:07:49 -0500

    Add initial scaffolding for AoC 2025

---
+++ aoc2025/d01.py
@@ -0,0 +1,17 @@
+def part1(lines):
+    return len(lines)
+
+
+def part2(lines):
+    return len(lines)
+
+
+def main():
+    with open("input/d01.txt", "r") as f:
+        lines = f.readlines()
+    print(part1(lines))
+    print(part2(lines))
+
+
+if __name__ == "__main__":
+    main()
