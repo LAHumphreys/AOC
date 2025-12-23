62746c32eaf78b83eb5328661cedc5475240801d
Luke.Humphreys <lhumphreys67@gmail.com>
2025-12-23 09:01:26 -0500

    2025 d02 - Part 2

--- aoc2025/d02.py
+++ aoc2025/d02.py
@@ -1,3 +1,6 @@
+from tools.string_operations import subdivide
+from typing import Optional
+
 def load_sample(file: str) -> list[tuple[str, str]]:
     data = []
     with open(file, "r") as f:
@@ -7,12 +10,25 @@ def load_sample(file: str) -> list[tuple[str, str]]:
             data.append((start, end))
     return data
 
-def is_invalid(digits: str) -> bool:
+def is_invalid(digits: str, group_size: Optional[int] = None) -> bool:
+    if group_size is None:
+        if len(digits) % 2 == 0:
+            group_size = len(digits)//2
+        else:
+            return False
+
     invalid = False
-    if len(digits) %2 == 0:
-        split_idx = len(digits)//2
-        invalid = (digits[:split_idx] == digits[split_idx:])
+    if len(digits) % group_size == 0:
+        parts = subdivide(digits, group_size)
+        base = parts.pop(0)
+        invalid = all(base == part for part in parts)
     return invalid
+
+def is_invalid_part_2(digits: str) -> bool:
+    for group_size in range(len(digits)//2, 0, -1):
+        if is_invalid(digits, group_size):
+            return True
+    return False
 
 def get_next_repeat(digits: str) -> str:
     if len(digits) %2 != 0:
@@ -22,6 +38,31 @@ def get_next_repeat(digits: str) -> str:
     if rhs < base:
         return base*2
     return str(int(base) +1)*2
+
+def get_first_invalid_of_len(str_len: int) -> str:
+    for group_size in range(str_len // 2, 0, -1):
+        if str_len % group_size == 0:
+            base = "1" + "0"*(group_size-1)
+            return base*(str_len//group_size)
+    return "1"*str_len
+
+
+def get_next_repeat_part_2(digits: str) -> str:
+    str_len = len(digits)
+    next_groups: list[int] = [int(get_first_invalid_of_len(str_len+1))]
+    for group_size in range(str_len // 2, 0, -1):
+        if str_len % group_size != 0:
+            continue
+        parts = subdivide(digits, group_size)
+        num_parts = len(parts)
+        base = parts.pop(0)
+        base_repeat = int(base*num_parts)
+        if base_repeat > int(digits):
+            next_groups.append(base_repeat)
+        else:
+            next_groups.append(int(str(int(base)+1)*num_parts))
+    return str(min(next_groups))
+
 
 def get_invalid_ids(start: str, end: str) -> list[int]:
     invalid_ids = []
@@ -34,6 +75,17 @@ def get_invalid_ids(start: str, end: str) -> list[int]:
         invalid_ids.append(end)
     return [int(x) for x in invalid_ids]
 
+def get_invalid_ids_part_2(start: str, end: str) -> list[int]:
+    invalid_ids = []
+    working = start
+    while int(working) < int(end):
+        if is_invalid_part_2(working):
+            invalid_ids.append(working)
+        working = get_next_repeat_part_2(working)
+    if is_invalid_part_2(end):
+        invalid_ids.append(end)
+    return [int(x) for x in invalid_ids]
+
 
 def part1(data: list[tuple[str, str]]) -> int:
     total = 0
@@ -41,13 +93,17 @@ def part1(data: list[tuple[str, str]]) -> int:
         total += sum(get_invalid_ids(start, end))
     return total
 
-def part2(lines: list[str]) -> int:
-    return 0
+def part2(data: list[tuple[str, str]]) -> int:
+    total = 0
+    for start, end in data:
+        total += sum(get_invalid_ids_part_2(start, end))
+    return total
 
 def main():
     try:
         data = load_sample("input/d02.txt")
         print(f"Part 1: {part1(data)}")
+        print(f"Part 1: {part2(data)}")
     except FileNotFoundError:
         print("Input file not found.")
 
--- tests/aoc2025Tests/test_aoc2025_d02.py
+++ tests/aoc2025Tests/test_aoc2025_d02.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from aoc2025.d02 import load_sample, get_next_repeat, get_invalid_ids, part1
+from aoc2025.d02 import  get_invalid_ids_part_2,  part2, get_next_repeat_part_2, is_invalid_part_2, is_invalid
 from tests.aoc2025Tests.aoc2025_common import get_test_file_path
 
 class TestDay02(TestCase):
@@ -15,10 +16,47 @@ class TestDay02(TestCase):
         self.assertEqual(get_next_repeat("222220"), "222222")
         self.assertEqual(get_next_repeat("1188511880"), "1188511885")
 
+    def test_simple_next_part_2(self):
+        self.assertEqual(get_next_repeat_part_2("10"), "11")
+        self.assertEqual(get_next_repeat_part_2("11"), "22")
+        self.assertEqual(get_next_repeat_part_2("95"), "99")
+        self.assertEqual(get_next_repeat_part_2("99"), "111")
+        self.assertEqual(get_next_repeat_part_2("998"), "999")
+        self.assertEqual(get_next_repeat_part_2("222220"), "222222")
+        self.assertEqual(get_next_repeat_part_2("1188511880"), "1188511885")
+        self.assertEqual(get_next_repeat_part_2("824824821"), "824824824")
+        self.assertEqual(get_next_repeat_part_2("2121212118"), "2121212121")
+
     def test_invalid_ids(self):
         self.assertEqual(get_invalid_ids("11", "22"), [11, 22])
         self.assertEqual(get_invalid_ids("95", "115"), [99])
         self.assertEqual(get_invalid_ids("998", "1012"), [1010])
         self.assertEqual(get_invalid_ids("1188511880", "1188511890"), [1188511885])
+
+    def test_invalid_ids_part_2(self):
+        self.assertEqual(get_invalid_ids_part_2("11", "22"), [11, 22])
+        self.assertEqual(get_invalid_ids_part_2("95", "115"), [99, 111])
+        self.assertEqual(get_invalid_ids_part_2("998", "1012"), [999, 1010])
+        self.assertEqual(get_invalid_ids_part_2("1188511880", "1188511890"), [1188511885])
+        self.assertEqual(get_invalid_ids_part_2("222220", "222224"), [222222])
+        self.assertEqual(get_invalid_ids_part_2("1698522", "1698528"), [])
+        self.assertEqual(get_invalid_ids_part_2("446443", "446449"), [446446])
+        self.assertEqual(get_invalid_ids_part_2("38593856", "38593862"), [38593859])
+        self.assertEqual(get_invalid_ids_part_2("565653", "565659"), [565656])
+        self.assertEqual(get_invalid_ids_part_2("824824821", "824824827"), [824824824])
+        self.assertEqual(get_invalid_ids_part_2("2121212118", "2121212124"), [2121212121])
+
+    def test_invalid_is_invalid(self):
+        self.assertEqual(is_invalid("99"), True)
+        self.assertEqual(is_invalid("99", 1), True)
+        self.assertEqual(is_invalid_part_2("99"), True)
+        self.assertEqual(is_invalid("111"), False)
+        self.assertEqual(is_invalid("111", 1), True)
+        self.assertEqual(is_invalid_part_2("111"), True)
+
+
     def test_part_one(self):
         self.assertEqual(part1(self.lines), 1227775554)
+
+    def test_part_two(self):
+        self.assertEqual(part2(self.lines), 4174379265)
